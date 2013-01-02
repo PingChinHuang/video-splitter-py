950bcadfd2e42dbb5873724d3ac3e232d393ab75
od710122@gmail.com <od710122@gmail.com@8ac9a055-9006-d6fe-06b8-d656c9c33849>
2013-01-02 10:46:36 +0000

    Implement OptionsField Implement ClipListField.btnAdd, ClipListField.btnClear, ClipListField.chkTimeFormat, ClipListField.calculateDuration
    Next:
    ClipListField.insertClipIntoList

--- tkinter_test.py
+++ tkinter_test.py
@@ -5,8 +5,10 @@ Created on 2013/1/1
 '''
 
 import ttk
+import re
 from tkinter import *
 from tkinter.filedialog import *
+from datetime import *
 
 class FileSelectionField:    
     def __init__(self, master, fmelbl_text):
@@ -39,7 +41,7 @@ class OptionsField:
         self.fmelbl.grid(sticky = N + W + E + S)
         self.fmelbl.grid_columnconfigure(1, weight = 1)
         
-        self.optionsText = ["New Name", "List Name", "mencoder arugments"]
+        self.optionsText = ["New Name", "List Name", "mencoder arguments"]
         for i in range(3):
             self.lbl = {self.optionsText[i] : 
                         ttk.Label(self.fmelbl, text = self.optionsText[i])}
@@ -51,8 +53,22 @@ class OptionsField:
             self.ent[self.optionsText[i]].grid(row = i, 
                                                column = 1, 
                                                sticky = N + W + E + S)
-        self.cmbox = ttk.Combobox(self.fmelbl)
+        
+        self.listFileName = StringVar()
+        self.ent["List Name"].configure(textvariable = self.listFileName)
+        self.listFileName.set('{filename}'.format(filename = datetime.today().strftime("%Y%m%d_%H%M")))
+        
+        self.argsList = ["-ofps 29.97 -vf harddup -ovc x264 -x264encopts bitrate=1000 -oac mp3lame -lameopts abr:br=128",
+                         "-ovc copy -oac mp3lame -lameopts abr:br=128",
+                         "-ofps 29.97 -vf harddup -ovc x264 -x264encopts bitrate=2500 -oac mp3lame -lameopts abr:br=128",
+                         "-ofps 29.97 -vf harddup -ovc x264 -x264encopts bitrate=3000 -oac pcm"]
+        self.defaultArgs = StringVar()
+        self.setDefaultArgs(0)
+        self.cmbox = ttk.Combobox(self.fmelbl, values = self.argsList, textvariable = self.defaultArgs)
         self.cmbox.grid(row = 2, column = 1, sticky = N + W + E + S)
+        
+    def setDefaultArgs(self, idx):
+        self.defaultArgs.set(self.argsList[idx])
     
 class ListField:
     def __init__(self, master, enVerticalScrbar = True):
@@ -106,20 +122,84 @@ class ClipListField(ListField):
         self.fme = ttk.Frame(self.lblfme)
         self.fme.grid(row = 0, column = 1, sticky = N + W + E + S)
         self.clipTimeText = ['Start', 'End', 'Duration']
+        self.clipTimeVar = {}
+        self.ent = {}
         for i in range(len(self.clipTimeText)):
             self.lblfme = {self.clipTimeText[i] : 
                            ttk.LabelFrame(self.fme, text = self.clipTimeText[i])}
             self.lblfme[self.clipTimeText[i]].pack()
-            self.ent = {self.clipTimeText[i] : 
-                        ttk.Entry(self.lblfme[self.clipTimeText[i]], width = 10)}
+            self.clipTimeVar[self.clipTimeText[i]] = StringVar()
+            self.ent[self.clipTimeText[i]] = ttk.Entry(self.lblfme[self.clipTimeText[i]], width = 10, textvariable = self.clipTimeVar[self.clipTimeText[i]])
             self.ent[self.clipTimeText[i]].pack()
         
         self.btnText = ['Add', 'Clear', 'Generate', 'Reset']
+        self.btn = {}
         for i in range(len(self.btnText)):
-            self.btn = {self.btnText[i] : 
-                        ttk.Button(self.fme, text = self.btnText[i], width = 9)}
+            self.btn[self.btnText[i]] = ttk.Button(self.fme, text = self.btnText[i], width = 9)
             self.btn[self.btnText[i]].pack()
+            
+        self.btn['Add'].configure(command = self.btnAdd)
+        self.btn['Clear'].configure(command = self.btnClear)
+        self.btn['Generate'].configure(command = self.btnGenerate)
+        self.btn['Reset'].configure(command = self.btnReset)
+        self.ent['Start'].bind('<Key-Return>', self.bindReturn)
+        self.ent['End'].bind('<Key-Return>', self.bindReturn)
+        self.ent['Duration'].configure(state = 'readonly')
         
+    def chkTimeFormat(self):
+        if self.clipTimeVar['Start'].get() == '':
+            print("self.clipTimeVar['Start'] is empty")
+            self.ent['Start'].focus()
+            return False
+        if self.clipTimeVar['End'].get() == '':
+            print("self.clipTimeVar['End'] is empty")
+            self.ent['End'].focus()
+            return False
+        if re.match("^([0-2][0-3])([0-5]\d){2}$", self.clipTimeVar['Start'].get()) == None:
+            self.ent['Start'].focus()
+            print("Incorrect Time Format(1)")
+            return False
+        if re.match("^([0-2][0-3])([0-5]\d){2}$", self.clipTimeVar['End'].get()) == None:
+            self.ent['End'].focus()
+            print("Incorrect Time Format(2)")
+            return False
+        return True
+    
+    def calculateDuration(self):
+        print("calculate clip duration")
+        startTime = datetime.strptime(self.clipTimeVar['Start'].get(), "%H%M%S")
+        endTime = datetime.strptime(self.clipTimeVar['End'].get(), "%H%M%S")
+        if endTime < startTime:
+            print("End time is less than start time")
+            return False
+        durationTime = endTime - startTime
+        self.clipTimeVar['Duration'].set(durationTime)
+        return True
+    
+    def insertClipIntoList(self):
+        pass
+
+    def btnAdd(self):
+        if not self.chkTimeFormat():
+            return
+        if not self.calculateDuration():
+            return
+        self.insertClipIntoList();
+    
+    def bindReturn(self, event):
+        self.btnAdd()
+    
+    def btnClear(self):
+        for i in range(len(self.clipTimeText)):
+            self.clipTimeVar[self.clipTimeText[i]].set('')
+        self.ent['Start'].focus()
+        print('btnClear')
+    
+    def btnGenerate(self):
+        pass
+    
+    def btnReset(self):
+        pass
         
 class ClipListGenerator:
     def __init__(self, master):
@@ -136,13 +216,15 @@ class ClipListGenerator:
                         ("All Files", "*"))
         self.fileSelectionField.setDialogDefaultFileTypes(fileTypeList)
         
+        
 class VideoSplitter:
     def __init__(self, master):
         master.grid_columnconfigure(0, weight = 1)
         master.grid_rowconfigure(1, weight = 1)
         self.fileSelectionField = FileSelectionField(master, "List File")
         self.clipListField = ListField(master)
-        self.progressField = ProgressField(master, 100, 'determinate', 'horizontal')
+        self.progressField = ProgressField(master, 100, 
+                                           'determinate', 'horizontal')
         self.btn = ttk.Button(self.progressField.fme, text = 'Start', 
                               width = 10,
                               command = self.btnStartSplitting)
